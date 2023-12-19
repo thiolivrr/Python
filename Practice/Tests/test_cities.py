23d6cfca9b6741b3f01102cb307bbb363d5c492f
thiolvrr <thiago.oliv0101@outlook.com>
2023-12-19 19:57:02 -0300

    toronto

--- Practice/Tests/test_cities.py
+++ Practice/Tests/test_cities.py
@@ -3,9 +3,9 @@ import pytest
 from city_functions import location
 
 def test_city_country():
-    place = location("Quebec","Canadá")
-    assert place == "Quebec, Canadá"
+    place = location("Toronto","Canadá")
+    assert place == "Toronto, Canadá"
 
 def test_city_country_population():
-    place = location("Quebec","Canadá", 50000)
-    assert place == "Quebec, Canadá - population 50000"
+    place = location("Toronto","Canadá", 50000)
+    assert place == "Toronto, Canadá - population 50000" 
