bef9136517913d2c1f2e009bcec21d9dea9fcf6c
Ac4stus <thiago.oliv0101@outlook.com>
2023-12-19 19:47:40 -0300

    pytest

--- Practice/Tests/city_functions.py
+++ Practice/Tests/city_functions.py
@@ -1,2 +1,7 @@
-def location(city, country):
-    print(f"{city.capitalize()}, {country.capitalize()}.")
+def location(city, country, population=0):
+    if population != 0:
+        place = (f"{city.capitalize()}, {country.capitalize()} - population {population}")
+    else:
+        place = (f"{city.capitalize()}, {country.capitalize()}")
+    
+    return place
--- Practice/Tests/test_cities.py
+++ Practice/Tests/test_cities.py
@@ -1,5 +1,11 @@
+import pytest
+
 from city_functions import location
 
 def test_city_country():
-    place = location("Quebec, Canadá")
-    assert place == ("Quebec, Canadá.")
+    place = location("Quebec","Canadá")
+    assert place == "Quebec, Canadá"
+
+def test_city_country_population():
+    place = location("Quebec","Canadá", 50000)
+    assert place == "Quebec, Canadá - population 50000"
