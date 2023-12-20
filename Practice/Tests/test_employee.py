9a2839f4feb4942fcb97ac7938fe618ec5b27ac3
Ac4stus <thiago.oliv0101@outlook.com>
2023-12-20 18:58:28 -0300

    Implementando fixture nas funções teste

--- Practice/Tests/test_employee.py
+++ Practice/Tests/test_employee.py
@@ -1,13 +1,17 @@
+import pytest
 import employee
 
-def test_give_default_raise():
-    william = employee.Employee('william', 'fernandes', 15000)
-    res = william.give_raise()
+@pytest.fixture
+def worker():
+    John = employee.Employee('John', 'Doe', 15000)
+    return John
+
+def test_give_default_raise(worker):
+    res = worker.give_raise()
     assert res == 'Seu sálario agora é 20000'
 
-def test_give_custom_raise():
-    william = employee.Employee('william', 'fernandes', 15000)
-    res = william.give_raise(15000)
+def test_give_custom_raise(worker):
+    res = worker.give_raise(15000)
     assert res == 'Seu sálario agora é 30000'
 
 
