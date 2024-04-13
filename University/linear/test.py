cbb6b7d22af381d29a7d518c2497ac8381deb5b4
Ac4stus <thiago.oliv0101@outlook.com>
2024-04-12 21:42:58 -0300

    matrix calc

--- University/linear/test.py
+++ University/linear/test.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 # Define the two matrices
-A = np.array([[1, 2, 9],
-              [0, 1, 6],
-              [0, 0, 1]])
-B = np.array([[1, -2, 3],
-              [0, 1, -6],
-              [0, 0, 1]])
+A = np.array([[0, 1, 1],
+              [0, 1, 0],
+              [1, 0, 1]])
+B = np.array([[-1, 1, 1],
+              [0, 1, 0],
+              [1, -1, 0]])
 
 # Multiply the matrices using numpy.dot
-
 result = np.dot(A, B)
 
 # Print the resulting product matrix
 print(result)
 
+# from numpy.linalg import inv
+# a = np.array([[1., 2.], [3., 4.]])
+# ainv = inv(a)
